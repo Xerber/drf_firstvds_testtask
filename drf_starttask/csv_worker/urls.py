bbb96ac4dd699012348e7991de828686b98d69ab
Igor Dziuba <cherobuk.91@gmail.com>
2022-08-11 00:19:48 +0300

    [Update] Добавлен второй и третий ендпоинты

--- drf_starttask/csv_worker/urls.py
+++ drf_starttask/csv_worker/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from csv_worker.views import CreateTaskAPIView
+from csv_worker.views import CreateTaskAPIView, QueueTaskAPIView, StatusTaskAPIView
 
 
 urlpatterns = [
     path('v1/create_task', CreateTaskAPIView.as_view()),
+    path('v1/queue_task', QueueTaskAPIView.as_view()),
+    path('v1/status_task', StatusTaskAPIView.as_view()),
 ]
--- drf_starttask/csv_worker/views.py
+++ drf_starttask/csv_worker/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from celery import current_app
 
 from .tasks import csv_task
 
@@ -7,6 +8,27 @@ from .tasks import csv_task
 
 class CreateTaskAPIView(APIView):
   def post(self,request, *args, **kwargs):
-    filename = request.data['filename']
+    filename = request.data.get('filename')
     task = csv_task.delay(filename)
     return Response({"success": f"Task '{task.id}' created successfully"})
+
+
+class QueueTaskAPIView(APIView):
+  def get(self, request):
+    active = current_app.control.inspect().active()
+    all_tasks = []
+    if active is not None:
+      for node, tasks in active.items():
+        all_tasks += tasks
+    return Response({'Count:': len(all_tasks)})
+
+
+class StatusTaskAPIView(APIView):
+  def post(self,request, *args, **kwargs):
+    task_id = request.data.get('task_id')
+    task = current_app.AsyncResult(task_id)
+    response_data = {'status_task': task.status, 'task_id': task.id}
+    if task.status == 'SUCCESS':
+      result = task.get()
+      response_data['result'] = result
+    return Response(response_data)
